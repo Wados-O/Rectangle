93ae134f0c666bec5e56468a9f2b08f4dd72a779
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-28 16:22:05 +0200

    Refactor RectangleTests for length and width

--- src/homework48/RectangleTest.java
+++ src/homework48/RectangleTest.java
@@ -6,20 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RectangleTest {
     @Test
-    public void getWidth(){
+    public void Width(){
 
         int sideA = 3;
         int sideB = 5;
+        int width = Math.min(sideA,sideB);
+
+
         Rectangle rect = new Rectangle(sideA,sideB);
 
-
-        int width = Math.min(sideA,sideB);
 
         assertEquals(width, rect.getWidth());
     }
     @Test
-    public void getLength(){
+    public void Length(){
 
+        int sideA = 3;
+        int sideB = 5;
+        int length = Math.max(sideA,sideB);
+        Rectangle rect = new Rectangle(sideA,sideB);
+
+
+
+
+        assertEquals(length, rect.getLength());
+    }
+    @Test
+    public void area(){
         int sideA = 3;
         int sideB = 5;
         Rectangle rect = new Rectangle(sideA,sideB);
