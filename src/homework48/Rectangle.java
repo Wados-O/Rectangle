149f99a164cbb1041c6e5757d203f78adb58e1bf
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-28 11:23:55 +0200

    add Rectangle.getArea()

--- src/homework48/Rectangle.java
+++ src/homework48/Rectangle.java
@@ -3,22 +3,29 @@ package homework48;
 public class Rectangle {
     public final int length;
     public final int width;
+    private final int area;
 
     public Rectangle(int a, int b) {
-        length = Math.max(a,b);
-        width = Math.min(a,b);
+        length = Math.max(a, b);
+        width = Math.min(a, b);
+        area = a * b;
 
     }
-    public int getLength(){
+
+    public int getLength() {
         return length;
     }
-    public int getWidth(){
+
+    public int getWidth() {
         return width;
+    }
+
+    public int getArea() {
+        return area;
     }
 
     @Override
     public String toString() {
-        return "Rectangle{length=" + length + ", width=" + width + "} (square: "  + width * length
-                + ")";
+        return "Rectangle{length=" + length + ", width=" + width + "} (square: " + area + ")";
     }
 }
--- src/homework48/RectangleAreaComparator.java
+++ src/homework48/RectangleAreaComparator.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 public class RectangleAreaComparator implements Comparator<Rectangle>{
     @Override
     public int compare(Rectangle o1, Rectangle o2) {
-        int area1 = o1.getLength() * o1.getWidth();
-        int area2 = o2.getLength() * o2.getWidth();
-        return area1 - area2;
+        return o1.getArea() - o2.getArea();
     }
 }
