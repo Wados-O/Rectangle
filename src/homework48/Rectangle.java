3a7558dc09a474e30564d7a99998833d7c1b98f6
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-28 11:12:51 +0200

    RectangleAreaComparator 1

--- src/homework48/Rectangle.java
+++ src/homework48/Rectangle.java
@@ -1,2 +1,23 @@
-package homework48;public class Rectangle {
+package homework48;
+
+public class Rectangle {
+    public final int length;
+    public final int width;
+
+    public Rectangle(int a, int b) {
+        length = Math.max(a,b);
+        width = Math.min(a,b);
+
+    }
+    public int getLength(){
+        return length;
+    }
+    public int getWidth(){
+        return width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{length=" + length + ", width=" + width + "}";
+    }
 }
